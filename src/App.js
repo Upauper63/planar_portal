cfca36d6de441b54e7bd318f9d4dc5e0fd837ecc
Upauper63 <upauper63@gmail.com>
2022-08-26 19:12:36 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import PlaneChase from './planechase.json'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const idx = Math.floor(Math.random() * PlaneChase.length);
+	const Plane = PlaneChase[idx];
+	const { innerWidth: width, innerHeight: height } = window;
+	console.log(width, height);
+  	return (
+		<div style={{'position': 'relative'}}>
+			<p style={{'background': 'white', 'opacity': '0.8', 'borderRadius': '40px', 'whiteSpace': 'preWrap', 'padding': '80px', 'position': 'absolute', 'top': '40%', 'left': '50%', 'transform': 'translate(-50%,-50%)', 'fontSize': '6em'}}>{`${Plane.text_jp}`}</p> */}
+			<div style={{ 'textAlign': 'center' }}>
+				<img style={{'width': '100%vh', 'height': '100vh', 'objectFit': 'contain'}} src={`${process.env.PUBLIC_URL}/img/${Plane.name.replace(/\s+/g, '')}.jpg`}/>
+			</div>
+		</div>	
+  	);
 }
 
 export default App;
