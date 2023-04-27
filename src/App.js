9f09d4b9f717dafb210172c5f42c3309bbdf1930
Upauper63 <upauper63@gmail.com>
2023-04-27 15:50:09 +0900

    [fix] text fontsize

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
 	const Plane = PlaneChase[idx];
   	return (
 		<div style={{'position': 'relative'}}>
-			<p style={{'background': 'white', 'opacity': '0.7', 'borderRadius': '8px', 'whiteSpace': 'preWrap', 'padding': '8px', 'position': 'absolute', 'top': '40%', 'left': '50%', 'transform': 'translate(-50%,-50%)', 'fontSize': '25px', 'width': '80%'}}>{`${Plane.text_jp}`}</p>
+			<p style={{'background': 'white', 'opacity': '0.7', 'borderRadius': '8px', 'whiteSpace': 'preWrap', 'padding': '8px', 'position': 'absolute', 'top': '40%', 'left': '50%', 'transform': 'translate(-50%,-50%)', 'fontSize': '16px', 'width': '80%'}}>{`${Plane.text_jp}`}</p>
 			<div style={{ 'textAlign': 'center' }}>
 				<img style={{'width': '100%'}} src={`${process.env.PUBLIC_URL}/img/${Plane.name.replace(/\s+/g, '')}.jpg`}/>
 			</div>
